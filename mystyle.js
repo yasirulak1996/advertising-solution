cce9d214c835323635b7c9c14414b4e24b4b8e36
yasirulak1996 <yasirulakshan96@gmail.com>
2024-08-16 12:12:02 +0530

    its now

---
+++ mystyle.js
@@ -0,0 +1,14 @@
+
+document.getElementById("b1").onclick =function(){ 
+    let sewa = document.getElementById("form1");
+
+       document.getElementById('idboost').innerHTML= sewa.innerHTML;}
+document.getElementById("b2").onclick = function(){
+
+let banda = document.getElementById("form2");
+
+       document.getElementById('idboost').innerHTML= banda.innerHTML;}
+
+
+
+            
